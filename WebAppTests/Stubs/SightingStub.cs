1caf388d116ccb3fd58e14b8c80ec813ad54e7d5
Tom <tom.beusch@stud.fh-campuswien.ac.at>
2022-04-26 10:53:35 +0200

    sighting 1 added

--- WebAppTests/Stubs/SightingStub.cs
+++ WebAppTests/Stubs/SightingStub.cs
@@ -1,7 +1,22 @@
-﻿namespace WebAppProftS2Tests.Stubs
+﻿using System.Collections.Generic;
+using InterfaceLayer.DTO;
+
+namespace WebAppProftS2Tests.Stubs
 {
     public class SightingStub
     {
-        
+        public List<SightingDTO> Sightings;
+
+        public SightingStub()
+        {
+            Sightings.Add(new SightingDTO()
+            {
+                _id = 1,
+                City = "Nijmegen", 
+                Street = "Mahoniastraat",
+                Lat = (float) 52.345,
+                Lng = (float) 5.345
+            });
+        }
     }
 }
